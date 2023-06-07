7a5b076af34fabda11e56ebd6c27ac889e582bc1
Wilson R <wijaro88@gmail.com>
2023-06-07 16:13:55 -0500

    final2

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
 // alert("running");
 let semaforoImg = document.getElementById("semaforo");
-// let semaforoImg = document.getElementById("semaforo");
 console.log(semaforoImg);
 let color = 'rojo';
 
 setInterval(() => {
   
   if (color === 'rojo') {
-    color = 'amarillo';
-    console.log('El semáforo cambió a AMARILLO');
-    
-  } else if (color === 'amarillo') {
     color = 'verde';
-    console.log('El semáforo cambió a VERDE');
-    
+  } else if (color === 'verde') {
+    color = 'amarillo'; 
   } else {
     color = 'rojo';
-    console.log('El semáforo cambió a ROJO');
-    
   }
-  
-  console.log(color);
-  // console.log(`/img/semaforo-${color}.png`);
-  semaforoImg.src =  ("/img/semaforo-" + color + ".png");
+   semaforoImg.src =  ("/img/semaforo-" + color + ".png");
 }, 3000);
 console
